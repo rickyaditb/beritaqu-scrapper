0c799a9ef046d03c37d6999ade73417c005d8689
rickyaditb <rickyaditb@gmail.com>
2024-03-06 15:16:50 +0700

    feat: add cookies mode

--- bing.py
+++ bing.py
@@ -1,12 +1,26 @@
 import g4f
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def summarizeNews(title, cookies):
-  print('Start Summarizing News...')
-  searchQuery = "rangkum berita dalam beberapa poin mengenai '" + title + "' ?"
-  response = g4f.ChatCompletion.create(
-      model=g4f.models.gpt_4,
-      messages=[{"role": "user", "content": searchQuery}],
-      provider=g4f.Provider.Bing,
-      cookies=cookies
-  )  
-  return(response)
+  if(os.getenv('MODE') == 0):
+    print('Start Summarizing News...')
+    searchQuery = "rangkum berita dalam beberapa poin mengenai '" + title + "' ?"
+    response = g4f.ChatCompletion.create(
+        model=g4f.models.gpt_4,
+        messages=[{"role": "user", "content": searchQuery}],
+        provider=g4f.Provider.Bing,
+    )  
+    return(response)
+  else:
+    print('Start Summarizing News...')
+    searchQuery = "rangkum berita dalam beberapa poin mengenai '" + title + "' ?"
+    response = g4f.ChatCompletion.create(
+        model=g4f.models.gpt_4,
+        messages=[{"role": "user", "content": searchQuery}],
+        provider=g4f.Provider.Bing,
+        cookies=cookies
+    )  
+    return(response)
